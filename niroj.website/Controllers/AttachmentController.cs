64ba8e806d82fdd9cf53518b849c106c24b6946b
developer.niroj <developer.niroj@gmail.com>
2021-08-06 08:21:41 +0545

    download resume

---
+++ niroj.website/Controllers/AttachmentController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace niroj.website.Controllers
+{
+    [Route("attachment")]
+    public class AttachmentController : Controller
+    {
+        private readonly IWebHostEnvironment _environment;
+        public AttachmentController(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+        [Route("resume")]
+        public IActionResult Index()
+        {
+            var baseDirectory = _environment.WebRootPath;
+            return new PhysicalFileResult($@"{baseDirectory}/attachments/Niroj_Dahal_Resume.pdf", "application/pdf");
+           // return View();
+        }
+    }
+}
