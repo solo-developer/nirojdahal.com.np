60009503a3840caac9ef59329f23aa287ab7e5ca
developer.niroj <developer.niroj@gmail.com>
2022-03-16 22:15:15 +0545

    blog category and base controller

---
+++ niroj.website/Areas/Admin/Controllers/BlogCategoryController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using niroj.website.Controllers;
+using Personal.Domain.Services.Interface;
+
+namespace niroj.website.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+    [Route("blog-category")]
+    public class BlogCategoryController : BaseController
+    {
+        private readonly IBlogCategoryService _blogCategoryService;
+        public BlogCategoryController(IBlogCategoryService blogCategoryService)
+        {
+            _blogCategoryService = blogCategoryService;
+        }
+
+        [HttpGet]
+        [Route("")]
+        [Route("index")]
+        public IActionResult Index()
+        {
+            var allCategories = _blogCategoryService.GetAll();
+            return View(allCategories);
+        }
+    }
+}
---
+++ niroj.website/Controllers/BaseController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace niroj.website.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected void clearUserCookies()
+        {
+            foreach (var cookie in Request.Cookies.Keys)
+            {
+                Response.Cookies.Delete(cookie);
+            }
+        }
+        protected string getLoggedInUserId()
+        {
+            string userId = Request.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            return userId;
+        }
+    }
+}
