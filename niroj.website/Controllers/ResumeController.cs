e1d9f4a96e9120e1edfdcf56d6e422a71a398023
developer.niroj <developer.niroj@gmail.com>
2022-03-20 21:02:35 +0545

    blog list

---
+++ niroj.website/Controllers/ResumeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Personal.Domain.Repository.Interface;
+
+namespace niroj.website.Controllers
+{
+    [Route("resume")]
+    public class ResumeController : Controller
+    {
+        private readonly ISettingRepository _settingRepo;
+        public ResumeController(ISettingRepository settingRepo)
+        {
+            _settingRepo = settingRepo;
+        }
+
+        [Route("")]
+        public IActionResult Index()
+        {
+            //var settings = _settingService.
+            return View();
+        }
+    }
+}
