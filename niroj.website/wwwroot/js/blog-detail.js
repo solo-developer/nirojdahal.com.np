1fcf06fd36e5f45817429c688be4a87d218158e5
developer.niroj <developer.niroj@gmail.com>
2022-03-25 20:34:43 +0545

    tags in blog

---
+++ niroj.website/wwwroot/js/blog-detail.js
@@ -0,0 +1,20 @@
+﻿(function () {
+    var d = document, s = d.createElement('script');
+
+    s.src = 'https://nirojdahal.disqus.com/embed.js';
+
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+})();
+
+
+//$(document).ready(function () {
+//    var disqusPublicKey = "YOUR_PUBLIC_KEY";
+//    var disqusShortname = "thenextweb";
+
+//    var urlArray = [];
+//    $('.count-comment').each(function () {
+//        var url = $(this).attr('data-disqus-url');
+//        urlArray.push('thread:link=' + url);
+//    });
+//});
